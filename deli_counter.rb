97fe62d321d9f66fc05359c3f0f6c6cd9b5c54eb
Alicia Yu <alicia.yu.16@gmail.com>
2017-10-04 00:53:51 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
 # Write your code here.
+def line(array)
+  if array.length == 2
+    return "#{array[0]} and #{array[1]}"
+    puts "The line is currently:#{array}"
+  elsif array.length > 2
+    array[-1].insert(0, "and ")
+    else 
+      puts "The line is currently empty."
+  end
+    array.join(", ")
+end
