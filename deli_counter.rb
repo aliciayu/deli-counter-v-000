6ac4cf4379ef4fd86ea8046cd14db1be3edad4d5
Alicia Yu <alicia.yu.16@gmail.com>
2017-10-04 01:04:48 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,22 +6,7 @@ def line(array)
     waiting = "The line is currently:"
     array.each.with_index(1) do |name, i|
       waiting << " #{i}. #{name}"
-
-    def line(deli)
-      if deli.empty?
-        puts "The line is currently empty."
-      else
-        current_line = "The line is currently:"
-        deli.each.with_index(1) do |person, i|
-          current_line << " #{i}. #{person}"
-        end
-        puts current_line
-      end
-    end
-    length == 2
-    puts "The line is currently: 1. #{ array[0]} 2. #{array[1}"
-  elsif array.length > 2
-    array[-1].insert(0)
-    else
-
+  end
+  puts current_line
+  end
 end
