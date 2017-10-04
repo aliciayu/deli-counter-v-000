f01fb3080f0bb8256ffeb4a84922c9c668ab87fb
Alicia Yu <alicia.yu.16@gmail.com>
2017-10-04 00:57:43 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,9 @@
 # Write your code here.
 def line(array)
   if array.length == 2
-    puts "The line is currently: 1. #{ array[0]} and 2. #{array[1}"
+    puts "The line is currently: 1. #{ array[0]} 2. #{array[1}"
   elsif array.length > 2
-    array[-1].insert(0, "and ")
+    array[-1].insert(0)
     else
       puts "The line is currently empty."
   end
