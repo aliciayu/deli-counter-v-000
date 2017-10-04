67af43edbe4520c9b266f03e70557517ee03a2a4
Alicia Yu <alicia.yu.16@gmail.com>
2017-10-04 01:06:08 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,10 +3,10 @@ def line(array)
   if array.empty?
     puts "The line is currently empty."
   else
-    waiting = "The line is currently:"
+    line_waiting = "The line is currently:"
     array.each.with_index(1) do |name, i|
-      waiting << " #{i}. #{name}"
+      line_waiting << " #{i}. #{name}"
   end
-  puts current_line
+  puts line_waiting
   end
 end
