8bf60da3ec53a777c3458a9fffe46e58660e837f
Alicia Yu <alicia.yu.16@gmail.com>
2017-10-04 01:13:51 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,5 +12,5 @@ def line(array)
 end
 
 def take_a_number(array, name)
-  puts "Welcome, #{name}. You are number #{array} in line."
+  puts "Welcome, #{name}. You are number #{array.lenth} in line."
 end
